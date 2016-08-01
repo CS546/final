c9182eceae66e20f40a96dbbd8409eebecb91f74
joshgrib <jgribbon@stevens.edu>
2016-07-31 20:49:30 -0400

    Forgot to add data stuff

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+const course_info = require("./course_info");
+
+module.exports = {
+    course_info: course_info
+};
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const data = require("../data");
+
 router.get("/", (req, res) => {
     res.render("layouts/home", {
         partial: "jquery-scripts"
