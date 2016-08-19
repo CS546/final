a09c61db3884579ce9e7c6cdded25cd7bf30f9a6
John-Pesenti <John-Pesenti@users.noreply.github.com>
2016-08-19 16:35:39 -0400

    new login stuff

---
+++ src/views/layouts/home.js
@@ -0,0 +1,72 @@
+"use strict"
+const express = require('express');
+const router = express.Router();
+
+const data = require("../data");
+
+router.get("/", (req, res) => {
+    let myData = data.course_info.getData();
+    myData.then( (data_result) => {
+        res.render("layouts/home", {
+            partial: "jquery-scripts",
+            placeholderData: data_result
+        });
+    });
+});
+
+router.post("/", (req, res) => {
+    let username = (req.body.username);
+    let password = (req.body.password);
+    console.log("username: ", username);
+    console.log("password: ", password);
+
+    data.course_info.getUserByName(username).then(user => {
+        if(password !== user.password) throw "Passwords don't match"
+        res.render("layouts/account", { partial: "jquery-scripts", user: user });
+    }).catch((e) => {
+        console.log("error");
+        res.render("layouts/home", { partial: "jquery-scripts", error: e });
+    });
+});
+
+router.get("/signup", (req, res) => {
+    let myData = data.course_info.getData();
+    myData.then( (data_result) => {
+        res.render("layouts/signup", {
+            partial: "jquery-scripts",
+            placeholderData: data_result
+        });
+    });
+});
+
+
+router.post("/signup", (req, res) => {
+    let email = (req.body.email);
+    let username = (req.body.username);
+    let password = (req.body.password);
+    let password2 = (req.body.password2);
+
+    console.log("password1: ", password);
+    console.log("password2: ", password2);
+
+    data.course_info.addUser(username, null, null, password, null, null, null, null, null).then(user => {
+        if(password !== password2) throw "Passwords don't match"
+        res.render("layouts/account", { partial: "jquery-scripts", user: user });
+    }).catch((e) => {
+        res.render("layouts/signup", { partial: "jquery-scripts", error: e });
+    });
+});
+
+router.get("/changePassword", (req, res) => {
+    let myData = data.course_info.getData();
+    myData.then( (data_result) => {
+        res.render("layouts/changePassword", {
+            partial: "jquery-scripts",
+            placeholderData: data_result
+        });
+    });
+});
+
+
+
+module.exports = router;
