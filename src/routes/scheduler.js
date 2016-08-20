89c3692f021893da55df8141ff1cb21f7d867174
Julia Kim <jkim19@stevens.edu>
2016-08-19 16:00:14 -1000

    added user to schedule

--- src/routes/scheduler.js
+++ src/routes/scheduler.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const sessionStorage = require('sessionStorage');
 var http = require('http');
 const router = express.Router();
 
 const data = require("../data");
 
 router.get("/", (req, res) => {
-    let myData = data.course_info.getData();
-    myData.then( (data_result) => {
-        res.render("layouts/scheduler_form", {
-            partial: "jquery-scripts",
-            course_entry_amount: 7
+    let id = sessionStorage.user_id;
+    data.course_info.getUserById(id).then(user => {
+        let myData = data.course_info.getData();
+        myData.then( (data_result) => {
+            res.render("layouts/scheduler_form", {
+                partial: "jquery-scripts",
+                course_entry_amount: 7,
+                user: user
+            });
         });
     });
 });
@@ -37,48 +42,52 @@ router.post("/", (req, res) => {
 });
 
 router.get("/schedule", (req, res) => {
-    /*
-    ex: /schedule?courses=CS 442,CS 392,CS 519,MA 3331
-    Respond with json data of the possible schedules
-    add "json=true" to querystring to get just the json back
-    */
-    //parse querystring and create new one on path, changing spaces to "%20"
-    courses = req.query.courses;
-    let path = '/get_combos?courses='
-    for(let i in courses){
-        let course = courses[i];
-        for(let char in course){
-            if(course[char] == ' '){
-                path += '%20';
-            }else{
-                path += course[char];
+    let id=sessionStorage.user_id;
+    data.course_info.getUserById(id).then(user => {
+        /*
+        ex: /schedule?courses=CS 442,CS 392,CS 519,MA 3331
+        Respond with json data of the possible schedules
+        add "json=true" to querystring to get just the json back
+        */
+        //parse querystring and create new one on path, changing spaces to "%20"
+        courses = req.query.courses;
+        let path = '/get_combos?courses='
+        for(let i in courses){
+            let course = courses[i];
+            for(let char in course){
+                if(course[char] == ' '){
+                    path += '%20';
+                }else{
+                    path += course[char];
+                }
             }
         }
-    }
-    //use the API I already have for finding schedules
-    let schedules = http.request( "http://dev.sitstuff.com" + path , (resp) => {
-        let sum_data = '';
-        resp.on('data', (data) => {//gets buffers
-            sum_data += data.toString('utf8');
-        });
-        resp.on('end', (data) => {//says when the stream of buffers is done
-            let json_bool = req.query.json;
-            if(json_bool){
-                res.json(JSON.parse(sum_data));
-            }else{
-                dataJSON = JSON.parse(sum_data);
-                schedules = [];
-                for(let i in dataJSON){
-                    schedules.push(dataJSON[i]);
+        //use the API I already have for finding schedules
+        let schedules = http.request( "http://dev.sitstuff.com" + path , (resp) => {
+            let sum_data = '';
+            resp.on('data', (data) => {//gets buffers
+                sum_data += data.toString('utf8');
+            });
+            resp.on('end', (data) => {//says when the stream of buffers is done
+                let json_bool = req.query.json;
+                if(json_bool){
+                    res.json(JSON.parse(sum_data));
+                }else{
+                    dataJSON = JSON.parse(sum_data);
+                    schedules = [];
+                    for(let i in dataJSON){
+                        schedules.push(dataJSON[i]);
+                    }
+                    //save schedules?
+                    res.render("layouts/scheduler", {
+                        partial: "save-schedule",
+                        schedules: schedules,
+                        user: user
+                    });
                 }
-                //save schedules?
-                res.render("layouts/scheduler", {
-                    partial: "save-schedule",
-                    schedules: schedules
-                });
-            }
-        });
-    }).end();
+            });
+        }).end();
+    });
 });
 
 router.post('/save', (req, res) => {
@@ -87,8 +96,10 @@ router.post('/save', (req, res) => {
     saveData = saveData.replace(")", "]");
     saveData = saveData.replace(/'/g, "\"");
     console.log(JSON.parse(saveData));
-    let userID = '';
-    data.course_info.addSchedule(userID, saveData);
+    let userID = sessionStorage.user_id;
+    data.course_info.addSchedule(userID, saveData).then((user) => {
+        console.log("user schedules: ", user.schedules);
+    });
 });
 
 module.exports = router;
